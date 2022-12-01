65692dcbaed9e4b0478f64a8c3c5fcf26e27699b
Nikita Koschin <nik.koshin@mail.ru>
2022-12-01 21:45:21 +0300

    Create SettingsActivity

---
+++ app/src/main/java/io/github/nhths/notionlivewallpaper/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package io.github.nhths.notionlivewallpaper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import io.github.nhths.notionlivewallpaper.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
