d0434fd97a3dfdfa7547b5d405946b64cea23d26
Nikita Koschin <nik.koshin@mail.ru>
2023-01-24 15:08:22 +0300

    Add Network utils class

---
+++ app/src/main/java/io/github/nhths/notionlivewallpaper/data/notion/NetwokUtils.kt
@@ -0,0 +1,24 @@
+package io.github.nhths.notionlivewallpaper.data.notion
+
+import android.util.Base64
+import okhttp3.MediaType.Companion.toMediaType
+
+class NetwokUtils {
+
+    enum class MediaTypes(private val type: String) {
+        JSON("application/json; charset=utf-8");
+
+        fun mediaType() = type.toMediaType()
+        fun getTypeString() = type
+    }
+
+    companion object {
+
+        fun makeBasicAuthCredentials(client_id: String, client_secret: String) =
+            "Basic " + Base64.encode(
+                "$client_id:$client_secret".toByteArray(),
+                Base64.NO_WRAP
+            ).toString(Charsets.UTF_8)
+
+    }
+}
