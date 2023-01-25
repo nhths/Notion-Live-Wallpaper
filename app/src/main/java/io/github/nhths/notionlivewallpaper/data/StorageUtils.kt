b754f7d397673db48b063b4ebb088171bff5ac13
Nikita Koschin <nik.koshin@mail.ru>
2023-01-25 18:05:45 +0300

    Add storage utils

---
+++ app/src/main/java/io/github/nhths/notionlivewallpaper/data/StorageUtils.kt
@@ -0,0 +1,9 @@
+package io.github.nhths.notionlivewallpaper.data
+
+class StorageUtils {
+    companion object {
+        val PREF_STORAGE_KEY = "AUTH_PREFS"
+        val PREF_KEY_TOKEN = "TOKEN"
+    }
+
+}
